22d4bcdf7ab3c9c5c3a1099477e7ea1ad225ff13
Taito Ohsumi <ohsumi-taito035@g.ecc.u-tokyo.ac.jp>
2024-07-07 10:11:39 +0900

    apply 'process_text()' to question sentence

--- main.py
+++ main.py
@@ -132,6 +132,7 @@ class VQADataset(torch.utils.data.Dataset):
         image = self.transform(image)
         question = np.zeros(len(self.idx2question) + 1)  # 未知語用の要素を追加
         question_words = self.df["question"][idx].split(" ")
+        question_words = process_text(self.df["question"][idx]).split(" ")
         for word in question_words:
             try:
                 question[self.question2idx[word]] = 1  # one-hot表現に変換
